280f4dd7684483e653d32c5f1b2759777533ed52
Rob Killick <robkillick@Robs-MBP-2.home>
2025-02-07 12:45:59 +0000

    implement autorisation for getting and creating teams

--- src/teams/api.py
+++ src/teams/api.py
@@ -2,6 +2,7 @@ from typing import List
 from django.shortcuts import get_object_or_404
 from ninja import Router
 
+import helpers
 from ninja_jwt.authentication import JWTAuth
 
 from .models import Team
@@ -15,21 +16,33 @@ router = Router()
 
 
 # /api/teams/
-@router.get("", response=List[TeamEntryListSchema], auth=JWTAuth())
+@router.get(
+    "", response=List[TeamEntryListSchema], auth=helpers.api_auth_user_required
+)
 def list_team_entries(request):
-    qs = Team.objects.all()
+    qs = Team.objects.filter(user=request.user)
     return qs
 
 
 # /api/teams/
-@router.post("", response=TeamEntryDetailSchema)
+@router.post(
+    "", response=TeamEntryDetailSchema, auth=helpers.api_auth_user_or_annon
+)
 def create_team_entry(request, data: TeamEntryCreateSchema):
-    print(data)
-    obj = Team.objects.create(**data.dict())
+    obj = Team(**data.dict())
+    print(request.user)
+    # obj.user = request.user
+    if request.user.is_authenticated:
+        obj.user = request.user
+    obj.save()
     return obj
 
 
-@router.get("{entry_id}/", response=TeamEntryDetailSchema, auth=JWTAuth())
+@router.get(
+    "{entry_id}/",
+    response=TeamEntryDetailSchema,
+    auth=helpers.api_auth_user_required,
+)
 def get_team_entry(request, entry_id: int):
-    obj = get_object_or_404(Team, id=entry_id)
+    obj = get_object_or_404(Team, id=entry_id, user=request.user)
     return obj
--- src/teams/models.py
+++ src/teams/models.py
@@ -1,10 +1,16 @@
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL  # "auth.User"
 
 
 # Create your models here.
 class Team(models.Model):
     """Team Model"""
 
+    user = models.ForeignKey(
+        User, default=None, null=True, blank=True, on_delete=models.SET_NULL
+    )
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
